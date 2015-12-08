ffe1f0ab4587af609f4845b385c21a8c36e31d69
Mikhail <m.s.sokolov.92@gmail.com>
2015-12-09 00:39:24 +0300

    Started functionality for adding Clients

--- MiniCRM/ViewModel/ClientListViewModel.cs
+++ MiniCRM/ViewModel/ClientListViewModel.cs
@@ -1,6 +1,8 @@
-﻿using MiniCRM.Repository;
+﻿using MiniCRM.Model;
+using MiniCRM.Repository;
 using System;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace MiniCRM.ViewModel
 {
@@ -8,10 +10,17 @@ namespace MiniCRM.ViewModel
     {
         readonly ClientRepository _clientRepository;
 
+        private ObservableCollection<Model.Client> _AllClients;
         public ObservableCollection<Model.Client> AllClients
         {
-            get;
-            private set;
+            get
+            {
+                return _AllClients;
+            }
+            private set
+            {
+                _AllClients = value;
+            }
         }
 
         public ClientListViewModel(ClientRepository clientRepository)
@@ -27,6 +36,33 @@ namespace MiniCRM.ViewModel
         protected override void OnDispose()
         {
             this.AllClients.Clear();
+        }
+
+
+        RelayCommand _invasionCommand;
+        public ICommand InvasionCommand
+        {
+            get
+            {
+                if (_invasionCommand == null)
+                {
+                    _invasionCommand = new RelayCommand(p => this.InvasionCommandExecute(), p => this.InvasionCommandCanExecute);
+                }
+                return _invasionCommand;
+            }
+        }
+        void InvasionCommandExecute()
+        {
+            Client cl = new Client("1", "2", "3", "4");
+            _clientRepository.Add(cl);
+            AllClients.Insert(AllClients.Count - 1, cl);
+        }
+        bool InvasionCommandCanExecute
+        {
+            get
+            {
+                return AllClients.Count.Equals(0) ? false : true;
+            }
         }
     }
 }
